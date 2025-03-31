26e3f67fc89f8adb6bbbe9a8446099733b2d5b45
NishantTheProgrammer <nishant@dhwaniris.com>
2025-03-31 23:50:01 +0530

    blue background

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="calculator">
-      <h1>Calculator</h1>
+      <h1>Calculator - blue</h1>
       <div className="calculator-grid">
         <input 
           type="number" 
