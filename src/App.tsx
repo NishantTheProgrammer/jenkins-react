5f57658c8c5f3b8c53cf6e944dfcd7efb72e22bf
NishantTheProgrammer <nishant@dhwaniris.com>
2025-03-31 10:39:56 +0530

    added intentional bug

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <h1>Calculator</h1>
       <input type="number" value={num1} onChange={(e) => setNum1(Number(e.target.value))} />
       <input type="number" value={num2} onChange={(e) => setNum2(Number(e.target.value))} />
-      <button onClick={() => setResult(num1 + num2)}>Add</button>
+      <button onClick={() => setResult(num1 * num2)}>Add</button>
       <p>Result: {result}</p>
     </>
   )
